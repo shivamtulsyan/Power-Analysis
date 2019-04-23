dbd211e5bc1d41e7062e875c17c6df583b29c7db
Shivam Tulsyan <shivtuls2009@gmail.com>
2019-04-23 20:43:53 +0530

    Add cubic equations

---
+++ cubiceqn1.m
@@ -0,0 +1,7 @@
+function [ y ] = cubiceqn1( x )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+y=(27/1000)*x^3-(27/100)*x^2+(27/40)*x;
+end
+
---
+++ cubiceqn2.m
@@ -0,0 +1,8 @@
+ function [ y ] = cubiceqn2( x)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+y=(81/4000)*x^3-(81/200)*x^2+(81/40)*x;
+
+end
+
